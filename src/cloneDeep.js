562a9d3ea959bb14c85ae70a3f1de44c177e0fd5
pfmao <ErenJagerM@outlook.com>
2022-01-05 01:02:42 +0800

    feat: add advanced cloneDeep

--- src/cloneDeep.js
+++ src/cloneDeep.js
@@ -19,6 +19,49 @@ function cloneDeep(obj) {
     return result;
 }
 
+/**
+ * Advanced Version
+ * ✅ Symbol
+ * ✅ Reference types like: Array, Function, Date, RegExp, Error...
+ * ✅ Non-enumerable properties
+ * ✅ Loop
+ */
+function cloneDeep(obj, cache = new WeakMap()) {
+    if (obj.constructor === Date) {
+        return new Date(obj);
+    }
+
+    if (obj.constructor === RegExp) {
+        return new RegExp(obj);
+    }
+
+    if (cache.has(obj)) return cache.get(obj);
+
+    const res = Object.create(
+        Object.getPrototypeOf(obj),
+        // NOTE: Copy non-enumerable properties
+        Object.getOwnPropertyDescriptors(obj)
+    );
+
+    cache.set(obj, res);
+
+    // NOTE: Reflect.ownKeys will return enumerable, non-enumerable, symbol properties
+    for (const key of Reflect.ownKeys(obj)) {
+        res[key] =
+            isComplexDataType(obj[key]) && typeof obj[key] !== 'function'
+                ? cloneDeep(obj[key], cache)
+                : obj[key];
+    }
+
+    return res;
+}
+
+function isComplexDataType(obj) {
+    return (
+        (typeof obj === 'object' || typeof obj === 'function') && obj !== null
+    );
+}
+
 // --------------Test--------------
 const obj = {
     null: null,
@@ -27,7 +70,7 @@ const obj = {
     string: 'abc',
     number: 1,
     bigInt: 1n,
-    // NOTE: Can't handle types below
+    // NOTE: Basic version can't handle types below
     symbol: Symbol(),
     object: { a: 1 },
     array: [1, 2, 3],
@@ -36,7 +79,13 @@ const obj = {
     regExp: new RegExp(),
     error: new Error('error'),
 };
-console.log(obj);
-console.log(cloneDeep(obj));
+Object.defineProperty(obj, 'innumerable', {
+    value: 'innumerable',
+    enumerable: false,
+});
 
-// TODO: Advanced Version
+obj.loop = obj;
+
+const clonedObj = cloneDeep(obj);
+console.log(obj);
+console.log(clonedObj);
